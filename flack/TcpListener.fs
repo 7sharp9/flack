e26d4b57ea67b1a80ec3ca49c937b79e21a54ff3
Dave Thomas <kukulcanenator@googlemail.com>
2011-05-02 10:07:06 +0100

    Changed send to use Buffer.BlockCopy as it is faster, modified test harness to send a msg every second with the server sending an initial 130 bytes on connect(this msg is currently not displayed in the client).

--- flack/TcpListener.fs
+++ flack/TcpListener.fs
@@ -76,7 +76,7 @@
                 connectedEvent.Trigger(endPoint)
                 args.AcceptSocket <- null (*remove the AcceptSocket because we will be reusing args*)
 
-                //start next accept
+                //start next accept, *note pool.CheckOut could block
                 do listeningSocket.AcceptAsyncSafe(completed, pool.CheckOut())
 
                 //check if data was given on connection
@@ -149,7 +149,8 @@
                             else remaining
                         let saea = pool.CheckOut()
                         saea.UserToken <- client
-                        Array.blit msg offset saea.Buffer saea.Offset tosend
+                        Buffer.BlockCopy(msg, offset, saea.Buffer, saea.Offset, tosend)
+                        //Array.blit msg offset saea.Buffer saea.Offset tosend
                         saea.SetBuffer(saea.Offset, tosend)
                         client.SendAsyncSafe(completed, saea)
                         loop (offset + tosend)
